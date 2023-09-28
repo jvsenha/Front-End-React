9b02878d015c47573f23567ea3a091f548d61393
jvsenha <joao.santos1381@etec.sp.gov.br>
2023-09-28 16:06:37 -0300

    Create Front-end

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    return (
+
+        <>
+            <h1>
+                login
+            </h1>
+            <Link to={"/"}> voltar para home</Link>
+        </>
+    );
+}
+
+export { Login };
