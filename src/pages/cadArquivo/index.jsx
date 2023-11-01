c08d462667a023aa3e5c49ec8def751d1dd12e7e
jvsenha <joao.santos1381@etec.sp.gov.br>
2023-11-01 08:56:43 -0300

    reposividade 3.0

--- src/pages/cadArquivo/index.jsx
+++ src/pages/cadArquivo/index.jsx
@@ -1,61 +1,89 @@
-import "./style.css"
+import "../../assets/style.css"
 import Sidebar from "../../components/Sidebar"
 import InputUpload from '../../components/InputUpload'
+import Input from '../../components/Input'
+import Button from '../../components/Button';
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 
 const CadArquivoEmp = () => {
-    const { pastaCliente } = useParams();
-    console.log(pastaCliente);
-    const [file, setFile] = useState(0);
-    const [setUploaded] = useState(false);
+  const { pastaCliente } = useParams();
+  console.log(pastaCliente);
+  const [file, setFile] = useState(0);
+  const [setUploaded] = useState(false);
 
-    const handleFileSelect = (e) => {
-        setFile(e.target.files[0]);
-    };
 
-    const handleUpload = async () => {
-        if (file) {
-          const formData = new FormData();
-          formData.append('file', file);
-         
-    
-          try {
-            const response = await fetch(`http://localhost:8000/upload/${pastaCliente}`, {
-              method: 'POST',
-              body: formData,
-            });
-    
-            if (response.ok) {
-              setUploaded(true);
-            } else {
-              console.error('Erro ao fazer o upload do arquivo.');
-            }
-          } catch (error) {
-            console.error('Erro ao fazer a solicitação:', error);
-          }
+  const Pasta = {
+    pastaCliente: "",
+  }
+  const [objPasta, setObjPasta] = useState(Pasta);
+
+
+  const [fileCount, setFileCount] = useState(0);
+
+  const handleFileChange = (e) => {
+    const selectedFiles = e.target.files;
+    setFileCount(selectedFiles.length + " file(s) selected");
+  };
+
+  const handleUpload = async () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+
+
+      try {
+        const response = await fetch(`http://localhost:8000/upload/${pastaCliente}`, {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          setUploaded(true);
+        } else {
+          console.error('Erro ao fazer o upload do arquivo.');
         }
-      };
+      } catch (error) {
+        console.error('Erro ao fazer a solicitação:', error);
+      }
+    }
+  };
 
-    return (
+  const digitar = (e) => {
+    setObjPasta({ ...objPasta, [e.target.name]: e.target.value });
+  }
+  return (
 
-        <>
+    <>
 
-            <Sidebar page="Cadastrar arquivo" />
+      <Sidebar page="Cadastrar arquivo" />
 
-            < div className="Main">
-                <form className="Form" >
+      < section className="main">
 
-                    <div className="input">
-                        <InputUpload onChange={handleFileSelect} funcao={handleUpload} className="input" placeholder="Pasta" label="Pasta" />
-                    </div>
-                </form>
-            </div>
+        <div className="upload">
+          <h1>
+            Upload de Arquivo
+          </h1>
+          <InputUpload Count={fileCount} onChange={handleFileChange} onClick={handleUpload} className="input" placeholder="Pasta" label="Pasta" />
+        </div>
 
-        </>
-    )
+        <div className="subpasta">
+          <h1 >
+            Criar subpasta
+          </h1>
+          <div className="input_subPasta">
+            <Input placeholder="Nome do cliente" label="Nome do cliente" name="nomeUser" eventoTeclado={digitar} obj={objPasta.pastaCliente} />
+          </div>
+          <div className="button">
+            <Button nome="Cadastrar" classname="Cadastrar"  />
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
 }
 
 
