8f376f95261414b8ab39d2752d860b5d21d2acb6
Nilton César Barbosa dos Santos <nilton.santos01@fatec.sp.gov.br>
2023-11-03 09:51:04 -0300

    AutoToken

--- src/pages/cadArquivo/index.jsx
+++ src/pages/cadArquivo/index.jsx
@@ -1,87 +1,118 @@
-import "../../assets/style.css"
-import Sidebar from "../../components/Sidebar"
-import InputUpload from '../../components/InputUpload'
-import Input from '../../components/Input'
-import Button from '../../components/Button';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
-
+import Sidebar from '../../components/Sidebar';
+import InputUpload from '../../components/InputUpload';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const CadArquivoEmp = () => {
-
+  const { pastaCliente } = useParams();
+  const [files, setFiles] = useState([]);
+  const [uploaded, setUploaded] = useState(false);
+  const [token, setToken] = useState(null);
 
   
 
-    const { pastaCliente } = useParams();
-    console.log(pastaCliente);
-    const [files, setFiles] = useState(0);
-    const [uploaded,setUploaded] = useState(false);
-const [fileCount, setFileCount] = useState(0);
+  const Pasta = {
+    subPasta: " ",
+  }
+  const [objPasta, setObjPasta] = useState(Pasta);
 
- const handleFileChange = (e) => {
+  const limparform = () => {
+    setObjPasta(Pasta)
+  }
+
+  const handleFileChange = (e) => {
     const selectedFiles = e.target.files;
-    setFileCount(selectedFiles.length + " file(s) selected");
+    setFiles(selectedFiles);
   };
 
-    const handleUpload = async () => {
-      if (files.length > 0) {
-        const formData = new FormData();
-        for (let i = 0; i < files.length; i++) {
-          formData.append('files', files[i]);
-        }
-       
-        try {
-          const response = await fetch(`http://localhost:8000/upload/${pastaCliente}`, {
-            method: 'POST',
-            body: formData,
-          });
-  
-          if (response.ok) {
-            setUploaded(true);
-          } else {
-            console.error('Erro ao fazer o upload dos arquivos.');
-          }
-        } catch (error) {
-          console.error('Erro ao fazer a solicitação:', error);
-        }
+  const handleUpload = async () => {
+    if (files.length > 0) {
+      const formData = new FormData();
+      for (let i = 0; i < files.length; i++) {
+        formData.append('files', files[i]);
       }
-    };
+
+      try {
+        const response = await fetch(`http://localhost:8000/upload/${pastaCliente}`, {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          setUploaded(true);
+        } else {
+          console.error('Erro ao fazer o upload dos arquivos.');
+        }
+      } catch (error) {
+        console.error('Erro ao fazer a solicitação:', error);
+      }
+    }
+  };
   
-  const digitar = (e) => {
-    setObjPasta({ ...objPasta, [e.target.name]: e.target.value });
+
+  const subPasta = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/subPasta/${pastaCliente}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+          subPasta: objPasta.subPasta})
+      });
+
+      if (response.ok) {
+        const pastaId = await response.text();
+        setObjPasta({... objPasta, subPasta:pastaId})
+        alert('Subpasta criada no Google Drive com sucesso!');
+      } else {
+        alert('Erro ao criar a subpasta no Google Drive.');
+      }
+    } catch (error) {
+      console.error('Erro durante o cadastro:', error);
+    }
+  };
+const digitar = (e) => {
+  setObjPasta({ ...objPasta, [e.target.name]: e.target.value });
+}; 
+useEffect(() => {
+  async function checkAuthentication() {
+    const response = await fetch('http://localhost:8000/check-auth');
+    if (response.ok) {
+      const data = await response.json();
+      setToken(data.token);
+    } else {
+      // Se o usuário não estiver autenticado, redirecione para autenticação
+      window.location.href = 'http://localhost:8000/auth/google';
+    }
   }
+  checkAuthentication();
+}, []);
+
 return (
-
-    <>
-
-      <Sidebar page="Cadastrar arquivo" />
-
-      < section className="main">
-
-        <div className="upload">
-          <h1>
-            Upload de Arquivo
-          </h1>
-          <InputUpload Count={fileCount} onChange={handleFileChange} onClick={handleUpload} className="input" placeholder="Pasta" label="Pasta" />
+  <>
+    <Sidebar page="Cadastrar arquivo" />
+    <section className="main">
+      <div className="upload">
+        <h1>Upload de Arquivo</h1>
+        <InputUpload Count={files.length + " file(s) selected"} onChange={handleFileChange} onClick={handleUpload} className="input" placeholder="Pasta" label="Pasta" />
+      </div>
+      <div className="subpasta">
+        <h1>Criar subpasta</h1>
+        <form action="">
+        <div className="input_subPasta">
+          <Input placeholder="Nome da pasta" label="Nome da pasta" name="subPasta" eventoTeclado={digitar} obj={objPasta.subPasta} />
         </div>
-
-        <div className="subpasta">
-          <h1 >
-            Criar subpasta
-          </h1>
-          <div className="input_subPasta">
-            <Input placeholder="Nome do cliente" label="Nome do cliente" name="nomeUser" eventoTeclado={digitar} obj={objPasta.pastaCliente} />
-          </div>
-          <div className="button">
-            <Button nome="Cadastrar" classname="Cadastrar"  />
-          </div>
+        <div className="button">
+          <Button nome="Criar Subpasta" classname="Cadastrar" funcao={subPasta} />
         </div>
-      </section>
-
-    </>
-  )
-}
-
+        </form>
+      </div>
+    </section>
+  </>
+);
+};
 
 export { CadArquivoEmp };
